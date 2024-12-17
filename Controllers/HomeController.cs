988a4c69f8a7de020e9f46fa3eb3a025006288de
Nintorch <136250445+beau13@users.noreply.github.com>
2024-12-17 19:10:00 +0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebTeploobmenApp.Data;
+using WebTeploobmenApp.Models;
+
+namespace WebTeploobmenApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TeploobmenContext _context;
+
+        public HomeController(ILogger<HomeController> logger, TeploobmenContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Variants.ToList());
+        }
+
+        public IActionResult Parameters(int id)
+        {
+            Variant variant = _context.Variants.Find(id) ?? new Variant();
+            Formulas.Data data = variant;
+            return View(data);
+        }
+
+        public IActionResult Calc(CalcViewModel viewModel)
+        {
+            int yCount = viewModel.H0 * 2 + 1;
+            double[,] result = new double[yCount, 8];
+
+            Func<Formulas.Data, double, double>[] funcs =
+            {
+                Formulas.CalcY,
+                Formulas.Calc1exp,
+                Formulas.Calc1mexp,
+                Formulas.CalcV,
+                Formulas.CalcO,
+                Formulas.CalcTMat,
+                Formulas.CalcTGas,
+                Formulas.CalcTDifference,
+            };
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int y = 0; y < yCount; y++)
+                {
+                    result[y, i] = Math.Round(funcs[i](viewModel, y / 2.0), 2);
+                }
+            }
+
+            viewModel.ResultTable = result;
+
+            _context.Variants.Add(new Variant(viewModel));
+            _context.SaveChanges();
+
+            return View(viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Formulas.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using WebTeploobmenApp.Models;
+
+namespace WebTeploobmenApp
+{
+    public static class Formulas
+    {
+        public struct Parameter
+        {
+            public string ReadableName { get; set; }
+            public string Name { get; set; }
+            public object Value { get; set; }
+
+            public Parameter(string readableName, string name, object value)
+            {
+                ReadableName = readableName;
+                Name = name;
+                Value = value;
+            }
+        }
+
+        public class Data
+        {
+            public int H0 { get; set; }
+            public double D { get; set; }
+            public double T1 { get; set; }
+            public double T2 { get; set; }
+            public double Wr { get; set; }
+            public double Cr { get; set; }
+            public double Cm { get; set; }
+            public double Gm { get; set; }
+            public double Av { get; set; }
+
+            public Data() { }
+            public Data(Data copy)
+            {
+                H0 = copy.H0;
+                D = copy.D;
+                T1 = copy.T1;
+                T2 = copy.T2;
+                Wr = copy.Wr;
+                Cr = copy.Cr;
+                Cm = copy.Cm;
+                Gm = copy.Gm;
+                Av = copy.Av;
+            }
+
+            public static List<Parameter> GetParameters(Data data = null) =>
+            [
+                new("Высота слоя", "h0", data?.H0),
+                new("Начальная температура окатышей", "t1", data?.T1),
+                new("Начальная температура воздуха", "t2", data?.T2),
+                new("Скорость воздуха на свободное сечение шахты", "wr", data?.Wr),
+                new("Средняя теплоёмкость воздуха при 200 градусах С", "cr", data?.Cr),
+                new("Расход окатышей", "cm", data?.Cm),
+                new("Теплоёмкость окатышей", "gm", data?.Gm),
+                new("Диаметр аппарата", "d", data?.D),
+                new("Величина альфа v", "av", data?.Av),
+            ];
+        }
+
+        public static double CalcM(Data data)
+        {
+            return (data.Gm * data.Cm) / (data.Wr * data.Cr * Math.PI * Math.Pow(data.D / 2, 2));
+        }
+
+        public static double CalcY(Data data, double y)
+        {
+            return (data.Av * y) / (data.Wr * data.Cr * 1000);
+        }
+
+        public static double Calc1exp(Data data, double y)
+        {
+            return 1 - Math.Exp((CalcM(data) - 1) * CalcY(data, y) / CalcM(data));
+        }
+
+        public static double Calc1mexp(Data data, double y)
+        {
+            return 1 - CalcM(data) * Math.Exp((CalcM(data) - 1) * CalcY(data, y) / CalcM(data));
+        }
+
+        public static double CalcV(Data data, double y)
+        {
+            return Calc1exp(data, y) / Calc1mexp(data, data.H0);
+        }
+
+        public static double CalcO(Data data, double y)
+        {
+            return Calc1mexp(data, y) / Calc1mexp(data, data.H0);
+        }
+
+        public static double CalcTMat(Data data, double y)
+        {
+            return data.T1 + (data.T2 - data.T1) * CalcV(data, y);
+        }
+
+        public static double CalcTGas(Data data, double y)
+        {
+            return data.T1 + (data.T2 - data.T1) * CalcO(data, y);
+        }
+
+        public static double CalcTDifference(Data data, double y)
+        {
+            return Math.Abs(CalcTMat(data, y) - CalcTGas(data, y));
+        }
+
+        private static readonly string[] resultNames =
+        {
+            "Y",
+            "1-exp",
+            "1-m*exp",
+            "v",
+            "o",
+            "Температура материала t, °C",
+            "Температура воздуха T, °C",
+            "Разность температур, °C",
+        };
+
+        public static string GetResultRowName(int id)
+        {
+            return resultNames[id];
+        }
+    }
+}
